294d4bc0139f417f1cbaf39d55578e51f76e0966
Thibault Seem <thibault.seem@heig-vd.ch>
2024-07-14 21:52:01 +0200

    Trying things

--- tb_app_rqm/lib/main.dart
+++ tb_app_rqm/lib/main.dart
@@ -1,8 +1,5 @@
-import 'dart:developer';
-import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:tb_app_rqm/Data/DossardData.dart';
 import 'package:tb_app_rqm/UI/InfoScreen.dart';
 import 'package:tb_app_rqm/UI/LoginScreen.dart';
@@ -12,14 +9,14 @@ import 'Data/NameData.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-
+/*
   if(await Permission.location.request().isGranted &&
       await Permission.locationAlways.request().isGranted){
     log('Location permission granted');
   } else {
     log('Location permission not granted');
     exit(0);
-  }
+  }*/
 
   final bool _isLoggedIn = await NameData.doesNameExist() && await DossardData.doesDossardExist();
 
