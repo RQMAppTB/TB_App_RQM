a6b03f5fa701c8b00733c46627f0aa7ab58a1921
Thibault Seem <thibault.seem@heig-vd.ch>
2024-08-30 09:35:37 +0200

    organized config a little bit

--- tb_app_rqm/lib/Utils/config.dart
+++ tb_app_rqm/lib/Utils/config.dart
@@ -3,6 +3,7 @@ import 'package:maps_toolkit/maps_toolkit.dart' as mp;
 /// Class to manage the configuration of the application.
 class Config {
   // ------------------- API -------------------
+  static const String SERVER_API ='rqm.iict-heig-vd.ch';
   static const String API_URL = 'rqm.iict-heig-vd.ch';
   static const String API_COMMON_ADDRESS = '/app/measures/';
   // -------------- Début et fin ---------------
@@ -22,6 +23,24 @@ class Config {
     mp.LatLng(LAT2, LON1),
     mp.LatLng(LAT2, LON2),
     mp.LatLng(LAT1, LON2),
+  ];
+  // ------------- Zone Défense ----------------
+  static const double LON11 = 6.657316176588863;
+  static const double LAT11 = 46.77812438877805;
+  static const double LON22 = 6.657231266655643;
+  static const double LAT22 = 46.782611095302414;
+  static const double LON33 = 6.666675613572716;
+  static const double LAT33 = 46.78271611345879;
+  static const double LON44 = 6.666769701835051;
+  static const double LAT44 = 46.778222689217166;
+  static const double LON55 = 6.653030810529115;
+  static const double LAT55 = 46.78007498916433;
+
+  static List<mp.LatLng> ZONE_DEFENSE = [
+    mp.LatLng(LAT11, LON11),
+    mp.LatLng(LAT22, LON22),
+    mp.LatLng(LAT33, LON33),
+    mp.LatLng(LAT44, LON44),
   ];
 
   // ----------------- Couleurs -----------------
